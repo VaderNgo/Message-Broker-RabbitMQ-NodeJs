174da3a7078f054411b31ac47d73b31cd2056e11
VaderNgo <vaderngo@gmail.com>
2024-07-01 14:47:50 +0700

    Initial Commit

---
+++ rabbitmq-nodejs/rabbit-sender/rabbit-sender.ts
@@ -0,0 +1,33 @@
+import amqp from "amqplib";
+
+const queue = "user-message";
+
+const message = {
+    message_id: "just-put-random-uuid-here",
+    content: "Hello World",
+};
+
+async function sendMessage() {
+    let connection;
+    try {
+        connection = await amqp.connect("amqp://localhost");
+
+        const channel = await connection.createChannel();
+
+        await channel.assertQueue(queue, { durable: false });
+
+        channel.sendToQueue(queue, Buffer.from(JSON.stringify(message)));
+
+        console.log(" [x] Sent %s", message);
+
+        await channel.close();
+    } catch (err) {
+        console.warn(err);
+    } finally {
+        if (connection) {
+            await connection.close();
+        }
+    }
+}
+
+sendMessage();
